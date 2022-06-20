18d48241429902b9bdae8b10bac37d9955f5fa34
zurum <zurumogbonda@gmail.com>
2022-06-20 22:19:02 +0100

    all good

---
+++ src/test/java/com/zurum/test/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package com.zurum.test.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void updateUserDetails() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+}
